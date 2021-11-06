c36276a441957023a8f170df6594d28358847de0
Yuya Kaido <yuyakaido@gmail.com>
2021-11-07 04:20:23 +0900

    Refactor Asset

--- src/main/kotlin/bitflyer/BitflyerService.kt
+++ src/main/kotlin/bitflyer/BitflyerService.kt
@@ -7,12 +7,12 @@ import kotlinx.serialization.ExperimentalSerializationApi
 object BitflyerService : Service {
 
     override suspend fun execute() {
-        val fiatDeposit = BitflyerDownloader.client.getFiatDepositHistory().map { it.toDepositRecord() }
-        val coinDeposit = BitflyerDownloader.client.getCoinDepositHistory().map { it.toDepositRecord() }
-        BitflyerExporter.exportDepositHistory(fiatDeposit + coinDeposit)
-        val fiatWithdraw = BitflyerDownloader.client.getFiatWithdrawHistory().map { it.toWithdrawRecord() }
-        val coinWithdraw = BitflyerDownloader.client.getCoinWithdrawHistory().map { it.toWithdrawRecord() }
-        BitflyerExporter.exportWithdrawHistory(fiatWithdraw + coinWithdraw)
+//        val fiatDeposit = BitflyerDownloader.client.getFiatDepositHistory().map { it.toDepositRecord() }
+//        val coinDeposit = BitflyerDownloader.client.getCoinDepositHistory().map { it.toDepositRecord() }
+//        BitflyerExporter.exportDepositHistory(fiatDeposit + coinDeposit)
+//        val fiatWithdraw = BitflyerDownloader.client.getFiatWithdrawHistory().map { it.toWithdrawRecord() }
+//        val coinWithdraw = BitflyerDownloader.client.getCoinWithdrawHistory().map { it.toWithdrawRecord() }
+//        BitflyerExporter.exportWithdrawHistory(fiatWithdraw + coinWithdraw)
         val trade = BitflyerImporter.importTradeRecords()
         BitflyerExporter.exportTradeHistory(trade)
     }
--- src/main/kotlin/model/Asset.kt
+++ src/main/kotlin/model/Asset.kt
@@ -1,31 +1,33 @@
 package model
 
-enum class Asset {
-    BTC,
-    XRP,
-    DOT,
-    BIT,
-    CBX,
-    MONA,
-    USD,
-    USDT,
-    JPY,
-    UNKNOWN;
-
+data class Asset(val value: String) {
     companion object {
+        private val BASE_ASSETS = listOf("BTC", "JPY", "USD", "USDT", "BUSD")
+        private fun sanitize(symbol: String): String {
+            return symbol.replace("/", "")
+        }
         fun pair(symbol: String): Pair<Asset, Asset> {
-            val first = values().first { symbol.startsWith(it.name) }
-            val second = values().first { symbol.endsWith(it.name) }
+            val sanitizedSymbol = sanitize(symbol)
+            val first = first(sanitizedSymbol)
+            val second = second(sanitizedSymbol)
             return first to second
         }
         fun single(asset: String): Asset {
-            return values().first { it.name == asset }
+            return Asset(asset)
         }
         fun first(symbol: String): Asset {
-            return values().first { symbol.startsWith(it.name) }
+            val sanitizedSymbol = sanitize(symbol)
+            val second = second(sanitizedSymbol)
+            val first = sanitizedSymbol.replace(second.value, "")
+            return Asset(first)
         }
         fun second(symbol: String): Asset {
-            return values().first { symbol.endsWith(it.name) }
+            val sanitizedSymbol = sanitize(symbol)
+            val second = BASE_ASSETS.first { sanitizedSymbol.endsWith(it) }
+            return Asset(second)
         }
+    }
+    override fun toString(): String {
+        return value
     }
 }
