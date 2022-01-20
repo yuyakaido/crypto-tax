abfa79c4c6ecf0288a5de2aa81cf8fd203cfd96b
Yuya Kaido <yuyakaido@gmail.com>
2022-01-20 22:09:37 +0900

    Fix TotalAverageMethod

--- src/main/kotlin/tax/TotalAverageMethod.kt
+++ src/main/kotlin/tax/TotalAverageMethod.kt
@@ -5,38 +5,60 @@ import kotlinx.serialization.ExperimentalSerializationApi
 import model.Asset
 import model.Side
 import java.math.BigDecimal
+import java.time.ZonedDateTime
 
 @ExperimentalSerializationApi
 object TotalAverageMethod {
 
+    private val btcJpyChartRecords = JsonImporter.importChartRecords("yuyakaido_btc_jpy_chart_history_2017")
+
+    private fun getNearestBtcJpyPrice(tradedAt: ZonedDateTime): BigDecimal {
+        val range = tradedAt to tradedAt.plusMinutes(5)
+        return btcJpyChartRecords.first {
+            range.first < it.date && it.date < range.second
+        }.price
+    }
+
     fun calculate() {
-//        val tradeRecords = JsonImporter.importTradeRecords("bitflyer_trade_history")
-//            .filter { it.tradedAt.year == 2017 }
-//            .filter { it.symbol.first == Asset.single("MONA") }
-//        val buyTradeRecords = tradeRecords
-//            .filter { it.side == Side.Buy }
-//        val sellTradeRecords = tradeRecords
-//            .filter { it.side == Side.Sell }
-//
-//        val totalPrice = buyTradeRecords
-//            .fold(BigDecimal.ZERO) { price, record ->
-//                price + record.tradePrice.multiply(record.tradeAmount)
-//            }
-//        val totalAmount = buyTradeRecords
-//            .fold(BigDecimal.ZERO) { amount, record ->
-//                amount + record.tradeAmount
-//            }
-//        println("TotalPrice = $totalPrice")
-//        println("TotalAmount = $totalAmount")
-//
-//        val totalAveragePrice = totalPrice.div(totalAmount)
-//        println("TotalAveragePrice = $totalAveragePrice")
-//
-//        sellTradeRecords
-//            .forEach { record ->
-//                val profitLoss = record.tradePrice.multiply(record.tradeAmount) - totalAveragePrice.multiply(record.tradeAmount)
-//                println("TradedAt: ${record.tradedAt}, Profit/Loss: $profitLoss")
-//            }
+        val bitflyerTradeRecords = JsonImporter.importTradeRecords("bitflyer_trade_history")
+        val bittrexTradeRecords = JsonImporter.importTradeRecords("bittrex_trade_history")
+        val poloniexTradeRecords = JsonImporter.importTradeRecords("poloniex_trade_history")
+        val allTradeRecords = bitflyerTradeRecords
+            .plus(bittrexTradeRecords)
+            .plus(poloniexTradeRecords)
+            .filter { it.tradedAt.year == 2017 }
+
+        val averagePrices = allTradeRecords
+            .groupBy { it.symbol.first }
+            .mapValues { entry ->
+                val buyTradeRecords = entry.value.filter { it.side == Side.Buy }
+                val totalAmount = buyTradeRecords.fold(BigDecimal.ZERO) { amount, record ->
+                    amount + record.tradeAmount
+                }
+                val totalCost = buyTradeRecords.fold(BigDecimal.ZERO) { cost, record ->
+                    cost + when (record.symbol.second) {
+                        Asset("JPY") -> record.tradePrice.multiply(record.tradeAmount)
+                        else -> {
+                            val costQuote = record.tradePrice.multiply(record.tradeAmount)
+                            costQuote.multiply(getNearestBtcJpyPrice(record.tradedAt))
+                        }
+                    }
+                }
+                return@mapValues totalCost.div(totalAmount)
+            }
+
+        averagePrices.forEach {
+            println("${it.key}: ¥${it.value}")
+        }
+
+        bitflyerTradeRecords
+            .filter { it.tradedAt.year == 2017 }
+            .filter { it.side == Side.Sell }
+            .forEach {
+                val averagePrice = averagePrices.getValue(it.symbol.first)
+                val profitLoss = it.tradePrice.multiply(it.tradeAmount) - averagePrice.multiply(it.tradeAmount)
+                println("${it.tradedAt}: ${it.symbol}: ¥$profitLoss")
+            }
     }
 
 }
